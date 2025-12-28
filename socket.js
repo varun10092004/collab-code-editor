2eb6e30114950eaf9a8c2f1da87b1482a9ec841e
varun10092004 <hemasaivarunnagam@gmail.com>
2025-12-28 18:47:07 +0530

    Implement error handling in socket events
    Added error handling for join-room and code-change events.

--- socket.js
+++ socket.js
@@ -5,21 +5,32 @@ module.exports = (io) => {
     console.log("User connected:", socket.id);
 
     socket.on("join-room", async ({ roomId }) => {
-      socket.join(roomId);
+      try {
+        socket.join(roomId);
 
-      let room = await Room.findOne({ roomId });
-      if (!room) room = await Room.create({ roomId, code: "" });
+        let room = await Room.findOne({ roomId });
+        if (!room) {
+          room = await Room.create({ roomId, code: "" });
+        }
 
-      socket.emit("sync-code", room.code);
+        socket.emit("sync-code", room.code);
+      } catch (error) {
+        console.error("JOIN ROOM ERROR:", error);
+      }
     });
 
     socket.on("code-change", async ({ roomId, code }) => {
-      await Room.findOneAndUpdate(
-        { roomId },
-        { code },
-        { upsert: true }
-      );
-      socket.to(roomId).emit("sync-code", code);
+      try {
+        await Room.findOneAndUpdate(
+          { roomId },
+          { code },
+          { upsert: true, new: true }
+        );
+
+        socket.to(roomId).emit("sync-code", code);
+      } catch (error) {
+        console.error("CODE UPDATE ERROR:", error);
+      }
     });
 
     socket.on("disconnect", () => {
@@ -27,3 +38,4 @@ module.exports = (io) => {
     });
   });
 };
+
